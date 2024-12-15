85dfa647020526326c492d365da8bef7b16ab877
Gia Vinh <vinhnguyen2762@gmail.com>
2024-12-15 20:30:53 +0700

    remove role from SmallTraderLocalStorageDto

--- people-service/src/main/java/people_service/dto/smallTrader/SmallTraderLocalStorageDto.java
+++ people-service/src/main/java/people_service/dto/smallTrader/SmallTraderLocalStorageDto.java
@@ -4,13 +4,11 @@ import people_service.model.SmallTrader;
 
 public record SmallTraderLocalStorageDto(
         Long id,
-        String role,
         String token
 ) {
     public static SmallTraderLocalStorageDto fromSmallTrader(SmallTrader smallTrader, String token) {
         return new SmallTraderLocalStorageDto(
                 smallTrader.getId(),
-                smallTrader.getRole().name(),
                 token
         );
     }
